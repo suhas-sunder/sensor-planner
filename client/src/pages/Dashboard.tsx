49fd0d7a68ed2975ebe295a20b4909d93bf4acc0
suhas <suhas@live.ca>
2025-07-15 21:46:12 -0400

    feat: new project
    -Store demo project elements in backup components. -Start adding elements for the actual project including a basic first floor block on home page.

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,9 @@
 export default function Dashboard() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <nav>
+        
+      </nav>
       <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
     </div>
   );
